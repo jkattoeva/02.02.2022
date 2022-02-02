2aec728007f06300717b1991ac66766407e62ba3
Janetta Kataoeva <jkattoeva@gmail.com>
2022-02-02 14:32:29 +0600

    start learn js

---
+++ script.js
@@ -0,0 +1,38 @@
+/* */ 
+// Variables
+var emptyVriable;
+var message = "Hello World";
+var firstName, lastName = "Imashow";
+var ;
+
+// Number
+// Javascript - математические операции не ведут к ошибке
+var exampleNumber5 = 123;
+var exampleNumber4 = 123.45;
+var exampleNumber3 = Infinity; // -Infinity
+var exampleNumber4 = NaN; // Not a Number
+var example = "Elaman" / 2;
+
+// document.write(typeof exampleNumber3)- выводит в браузер
+
+// String
+var exampleString1 = "Hello world";
+var exampleString2 = 'Hello world';
+var exampleString3 = `Hello world`;
+var exampleString4 = "4";
+
+// Boolean 
+var exampleBoolean1 = true;
+var exampleBoolean2 = false;
+
+// Special types
+var exampleNul = null; //
+var exampleUnderfine1d; 
+var exampleUnderfined2 = undefined; //незадано
+
+//BigInt - Not now 
+var exampleBigInt = 2122645545435543445n; //большое число
+// Symbol - Not now
+var exampleSymbol = Symbol("id");
+
+
