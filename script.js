41de986a63d3fe535f007ab451d8ae0b2d0e1a7a
Janetta Kataoeva <jkattoeva@gmail.com>
2022-02-02 15:46:50 +0600

    created question for user

--- script.js
+++ script.js
@@ -1,94 +1,173 @@
-/* */ 
-// Variables
-var emptyVriable;
-var message = "Hello World";
-var firstName, lastName = "Imashow";
-var ;
+// /* */ 
+// // Variables
+// var emptyVriable;
+// var message = "Hello World";
+// var firstName, lastName = "Imashow";
+// var ;
 
-// Number
-// Javascript - математические операции не ведут к ошибке
-var exampleNumber5 = 123;
-var exampleNumber4 = 123.45;
-var exampleNumber3 = Infinity; // -Infinity
-var exampleNumber4 = NaN; // Not a Number
-var example = "Elaman" / 2;
+// // Number
+// // Javascript - математические операции не ведут к ошибке
+// var exampleNumber5 = 123;
+// var exampleNumber4 = 123.45;
+// var exampleNumber3 = Infinity; // -Infinity
+// var exampleNumber4 = NaN; // Not a Number
+// var example = "Elaman" / 2;
 
-// document.write(typeof exampleNumber3)- выводит в браузер
+// // document.write(typeof exampleNumber3)- выводит в браузер
 
-// String
-var exampleString1 = "Hello world";
-var exampleString2 = 'Hello world';
-var exampleString3 = `Hello world`;
-var exampleString4 = "4";
+// // String
+// var exampleString1 = "Hello world";
+// var exampleString2 = 'Hello world';
+// var exampleString3 = `Hello world`;
+// var exampleString4 = "4";
 
-// Boolean 
-var exampleBoolean1 = true;
-var exampleBoolean2 = false;
+// // Boolean 
+// var exampleBoolean1 = true;
+// var exampleBoolean2 = false;
 
-// Special types
-var exampleNul = null; //
-var exampleUnderfine1d; 
-var exampleUnderfined2 = undefined; //незадано
+// // Special types
+// var exampleNul = null; //
+// var exampleUnderfine1d; 
+// var exampleUnderfined2 = undefined; //незадано
 
-//BigInt - Not now 
-var exampleBigInt = 2122645545435543445n; //большое число
-// Symbol - Not now
-var exampleSymbol = Symbol("id");
+// //BigInt - Not now 
+// var exampleBigInt = 2122645545435543445n; //большое число
+// // Symbol - Not now
+// var exampleSymbol = Symbol("id");
 
-// Objects
-var exampleObject1 = {};
-var exampleObject2 = Math;
+// // Objects
+// var exampleObject1 = {};
+// var exampleObject2 = Math;
 
-// Weird stuff - странные вещи
-documen.write(typeof null); //object
+// // Weird stuff - странные вещи
+// documen.write(typeof null); //object
 
-//Output
-// alert("Hello world");
-// var result = promt("Hello world"); string
-// var result = confirm(Hello world); boolean 
-//document.write("Hello world");
+// //Output
+// // alert("Hello world");
+// // var result = promt("Hello world"); string
+// // var result = confirm(Hello world); boolean 
+// //document.write("Hello world");
 
-var exampleTypeConversion1 = 5 + "5";
-var exampleTypeConversion2 = 5 + 5 + "5";
-var exampleTypeConversion3 = true + 2;
-var exampleTypeConversion4 = new String(5);
-var exampleTypeConversion5 = new Number("10")
-var exampleTypeConversion6 = new Boolean(" ");
+// var exampleTypeConversion1 = 5 + "5";
+// var exampleTypeConversion2 = 5 + 5 + "5";
+// var exampleTypeConversion3 = true + 2;
+// var exampleTypeConversion4 = new String(5);
+// var exampleTypeConversion5 = new Number("10")
+// var exampleTypeConversion6 = new Boolean(" ");
 
-// Math
-// Operand x = 5 + 6;
-// Operands: 5, 6
-// Operattor: +
+// // Math
+// // Operand x = 5 + 6;
+// // Operands: 5, 6
+// // Operattor: +
 
-// Unary operator
-var x = 5;
-x = -x;
+// // Unary operator
+// var x = 5;
+// x = -x;
 
-// var y = "3";
-// y = +y;
+// // var y = "3";
+// // y = +y;
 
-//Binary operator
-var  x = 1, y = 3;
-y = x + y;
-
-
-var exampleMath1 = 5 + 6;
-var exampleMath2 = 5 - 6;
-var exampleMath3 = 5 / 6;
-var exampleMath4 = 5 * 6;
-
-// Remainder 
-var exampleMath5 = 5 % 2; //1 нечетное
-var exampleMath6 = 6 % 2; //0 четное
-
- // Exponent
- var exampleMath7 = 2 ** 4;
-
-//  document.write("Hello" + "World");
-// document.write(5 * (2 - 3) - 5);
+// //Binary operator
+// var  x = 1, y = 3;
+// y = x + y;
 
 
-// 
+// var exampleMath1 = 5 + 6;
+// var exampleMath2 = 5 - 6;
+// var exampleMath3 = 5 / 6;
+// var exampleMath4 = 5 * 6;
+
+// // Remainder 
+// var exampleMath5 = 5 % 2; //1 нечетное
+// var exampleMath6 = 6 % 2; //0 четное
+
+//  // Exponent
+//  var exampleMath7 = 2 ** 4;
+
+// //  document.write("Hello" + "World");
+// // document.write(5 * (2 - 3) - 5);
+
+
+// // Fast modify
+// var exampleMath8 = 4;
+// exampleMath8 += 5;
+// exampleMath8 -= 3; // exampleMth8 = exampleMath8 - 3;
+
+// // Increment / decrement
+// var exampleMath9 = 0;
+// exampleMath9++;
+// exampleMath9--;
+// ++exampleMath9;
+// --exampleMath9;
+
+// // Comparisons
+// var exampleComparison1 = 6 > 5; //true
+// var exampleComparison2 = 6 < 5; // false
+// var exampleComparison3 = 6 >= 5; //true
+// var exampleComparison4 = 6 <= 5; //false
+// var exampleComparison5 = 5 === 5; //false
+// var exampleComparison6 = 5 !== 5; //false
+// var exampleComparison7 = "5" == 5; //true
+// var exampleComparison8 = "5" != 5; //true
+
+// // document.write("5" == 5);
+
+// // if else 
+// /*
+// Если чайник кипит
+//  Выключить газ
+// Иначе
+//  Проыерить позже
+// */
+
+// if (isKettkeBoling()) {
+//   switchGasOff();
+// }
+// else {
+//   checklater();
+// }
+
+// var age = promt ("How old are you&");
+// var votingYears = age - 18;
+// if (votingYears === NaN) {
+//   alert("Wrong input . Numbers only .");
+// }
+// else if (votingYears < 0) {
+//   alert("You are too young to vote!");
+// }
+// else {
+//   alert("You could've vote for" + votingYears + "years");
+// }
+
+/*
+Если не пользоатель ввел число 13
+ спросить снова
+*/ 
+// var userNumber; //inderfined
+// while (userNumber != "3.14") {
+//   userNumber = prompt("What is the number PI?");
+// }
+
+/*
+Начни с числа 1
+Увеличивать число на единицу
+Пока число меньше или равно 3 
+*/
+for (var index = 1; index <= 3; index++) {
+  document.write(index);
+}
+
+
+
+// var votingYears = age - 18;
+// alert("You can vote for" + votingYears + "years");
+
+// if (5 > 3) {
+//   document.write("Hello world");
+// }
+// else {
+//   document.write("Bye world");
+// }
 
 
 
